86ffeed95e49ee4bbbed54b171fab08c392f5551
Giankrp <gianruiz2003@gmail.com>
2023-09-02 22:03:06 -0500

    Drawer add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Tasks from "./components/Tasks/Tasks.jsx";
 import { Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage.jsx";
 import Register from "./components/Registrer/Registrer.jsx";
+import Drawer from "./components/Drawer/Drawer.jsx";
 
 const App = () => {
   // const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/tasks" element={<Tasks />} />
         <Route path="/HomePage" element={<HomePage />} />
-        <Route path="Register" element={<Register />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Drawer" element={<Drawer />} />
       </Routes>
     </div>
   );
---
+++ src/components/Drawer/Drawer.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+const Drawer = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      {/* Botón para mostrar/ocultar el cajón */}
+      <div className="text-center">
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          type="button"
+          onClick={toggleDrawer}
+        >
+          {isOpen ? "Hide navigation" : "Show navigation"}
+        </button>
+      </div>
+
+      {/* Contenido del cajón */}
+      <div
+        className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } bg-white w-64 dark:bg-gray-800`}
+        tabIndex="-1"
+      >
+        {/* Botón para cerrar el cajón */}
+        <button
+          type="button"
+          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 right-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+          onClick={toggleDrawer}
+        >
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span className="sr-only">Close menu</span>
+        </button>
+
+        {/* Contenido del cajón */}
+        <h5 className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">
+          Menu
+        </h5>
+        <div className="py-4 overflow-y-auto">
+          {/* Aquí puedes agregar los elementos de menú */}
+          {/* Por ejemplo: */}
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ml-3">Dashboard</span>
+              </a>
+            </li>
+            {/* Agrega más elementos de menú aquí */}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Drawer;
